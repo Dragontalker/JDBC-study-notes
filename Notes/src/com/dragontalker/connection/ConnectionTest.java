876a46d54411e90bc43e9ea4fd45168cf165c0ba
Richard <richard.yang.tong@gmail.com>
2021-05-29 21:10:10 -0400

    Added notes.

--- Notes/src/com/dragontalker/connection/ConnectionTest.java
+++ Notes/src/com/dragontalker/connection/ConnectionTest.java
@@ -1,4 +1,24 @@
 package com.dragontalker.connection;
 
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.util.Properties;
+
 public class ConnectionTest {
+
+    @Test
+    public void test1() {
+
+        Driver driver = null;
+
+        String url = null;
+        Properties info = null;
+
+        //Connection conn = driver.connection(url, info);
+
+        //System.out.println(conn);
+    }
+
 }
