24b738fcfb2faead51b90e3ab82d6b67cc5c3a6b
Richard <richard.yang.tong@gmail.com>
2021-05-29 21:16:38 -0400

    Added notes.

--- Notes/src/com/dragontalker/connection/ConnectionTest.java
+++ Notes/src/com/dragontalker/connection/ConnectionTest.java
@@ -4,14 +4,15 @@ import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.Driver;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionTest {
 
     @Test
-    public void test1() {
+    public void test1() throws SQLException {
 
-        Driver driver = null;
+        Driver driver = new com.mysql.jdbc.Driver();
 
         String url = null;
         Properties info = null;
