b2aad14607ec024e8ad4a47c98d74441ef1d917c
Richard <richard.yang.tong@gmail.com>
2021-06-08 07:57:02 -0400

    Added notes.

--- Notes/src/com/dragontalker/statement/crud/User.java
+++ Notes/src/com/dragontalker/statement/crud/User.java
@@ -5,4 +5,33 @@ public class User {
     private String password;
 
     public User() {}
+
+    public User(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user='" + user + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
