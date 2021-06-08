ead13d63e190f762365e1d82a1f4030aa80662d3
Richard <richard.yang.tong@gmail.com>
2021-06-08 07:58:54 -0400

    Added notes.

---
+++ Notes/src/com/dragontalker/statement/crud/StatementTest.java
@@ -0,0 +1,12 @@
+package com.dragontalker.statement.crud;
+
+import org.junit.Test;
+
+public class StatementTest {
+
+    //使用Statement的弊端: 需要拼写sql语句, 并且存在SQL注入的问题
+    @Test
+    public void testLogin() {
+
+    }
+}
