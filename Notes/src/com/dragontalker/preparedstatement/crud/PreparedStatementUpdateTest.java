add55bba0bc7493a9bf4db2d6a4fa8fbf5284831
Richard <richard.yang.tong@gmail.com>
2021-06-11 19:32:54 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/PreparedStatementUpdateTest.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/PreparedStatementUpdateTest.java
@@ -2,6 +2,9 @@ package com.dragontalker.preparedstatement.crud;
 
 /**
  * 使用PreparedStatement来替换Statement, 实现对数据表的增删改查操作
+ *
+ * 增删改(广义上的修改): 没有return
+ * 查: 有return
  */
 
 public class PreparedStatementUpdateTest {
