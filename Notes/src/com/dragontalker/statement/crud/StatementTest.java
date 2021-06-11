1055a854b816e9d68e680950b6ee132480db6308
Richard <richard.yang.tong@gmail.com>
2021-06-11 19:24:18 -0400

    Added notes.

--- Notes/src/com/dragontalker/statement/crud/StatementTest.java
+++ Notes/src/com/dragontalker/statement/crud/StatementTest.java
@@ -9,6 +9,8 @@ public class StatementTest {
     //使用Statement的弊端: 需要拼写sql语句, 并且存在SQL注入的问题
     @Test
     public void testLogin() {
+        //如何避免sql注入:
+        //只要用PreparedStatement(从Statement扩展而来) 取代Statement
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("请输入用户名: ");
