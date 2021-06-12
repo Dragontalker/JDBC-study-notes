ae6f90d2e188ea795b3720757c67c446a51fceb7
Richard <richard.yang.tong@gmail.com>
2021-06-11 20:57:21 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/CustomerForQuery.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/CustomerForQuery.java
@@ -16,29 +16,37 @@ import java.sql.ResultSet;
 public class CustomerForQuery {
 
     @Test
-    public void testQuery1() throws Exception{
-        Connection conn = JDBCUtils.getConnection();
-        String sql = "select id, name, email, birth from customers where id = ?";
-        PreparedStatement ps = conn.prepareStatement(sql);
+    public void testQuery1() {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            String sql = "select id, name, email, birth from customers where id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setObject(1, 1);
 
-        //执行并返回结果集
-        ResultSet resultSet = ps.executeQuery();
+            //执行并返回结果集
+            resultSet = ps.executeQuery();
 
-        //处理结果集
-        if(resultSet.next()) {//判断结果集的下一条是否有数据, 如果有数据, 返回true并指针下移, 如果false直接结束
+            //处理结果集
+            if(resultSet.next()) {//判断结果集的下一条是否有数据, 如果有数据, 返回true并指针下移, 如果false直接结束
 
-            //获取当前数据各个字段值
-            int id = resultSet.getInt(1);
-            String name = resultSet.getString(2);
-            String email = resultSet.getString(3);
-            Date birth = resultSet.getDate(4);
+                //获取当前数据各个字段值
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
+                String email = resultSet.getString(3);
+                Date birth = resultSet.getDate(4);
 
-            //将数据封装成一个对象
-            Customer customer = new Customer(id, name, email, birth);
-            System.out.println(customer);
+                //将数据封装成一个对象
+                Customer customer = new Customer(id, name, email, birth);
+                System.out.println(customer);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            //关闭资源
+            JDBCUtils.closeResource(conn, ps, resultSet);
         }
-
-        //关闭资源
-
     }
 }
