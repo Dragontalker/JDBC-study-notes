5393c6e4ff0d329b13ab3f69118c78b59dcf543f
Richard <richard.yang.tong@gmail.com>
2021-06-11 20:51:20 -0400

    Added notes.

--- Notes/src/com/dragontalker/bean/Customer.java
+++ Notes/src/com/dragontalker/bean/Customer.java
@@ -1,5 +1,13 @@
 package com.dragontalker.bean;
 
+/**
+ * ORM编程思想(Object Relational Mapping)
+ * 一个数据表对应一个Java类
+ * 表中的一条记录对应Java类的一个对象
+ * 表中的一个字段对应Java类的一个属性
+ */
+
 public class Customer {
+    private int id;
 
 }
