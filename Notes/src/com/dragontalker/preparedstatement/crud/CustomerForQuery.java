16a1c8a14e68ebf756b127a25012a70d1de53ed9
Richard <richard.yang.tong@gmail.com>
2021-06-11 20:34:06 -0400

    Added notes.

---
+++ Notes/src/com/dragontalker/preparedstatement/crud/CustomerForQuery.java
@@ -0,0 +1,15 @@
+package com.dragontalker.preparedstatement.crud;
+
+import org.junit.Test;
+
+/**
+ * 针对于Customer表的查询操作
+ */
+
+public class CustomerForQuery {
+
+    @Test
+    public void testQuery1() {
+
+    }
+}
