e79619983cc23e1bc92f5356a9ada6d43c01bb11
Richard <richard.yang.tong@gmail.com>
2021-06-11 20:43:14 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/CustomerForQuery.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/CustomerForQuery.java
@@ -4,6 +4,7 @@ import com.dragontalker.util.JDBCUtils;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -21,5 +22,16 @@ public class CustomerForQuery {
 
         //执行并返回结果集
         ResultSet resultSet = ps.executeQuery();
+
+        //处理结果集
+        if(resultSet.next()) {//判断结果集的下一条是否有数据, 如果有数据, 返回true并指针下移, 如果false直接结束
+
+            //获取当前数据各个字段值
+            int id = resultSet.getInt(1);
+            String name = resultSet.getString(2);
+            String email = resultSet.getString(3);
+            Date birth = resultSet.getDate(4);
+
+        }
     }
 }
