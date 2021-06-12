86b1a9361971c4a6d968d79df41a484c521ae878
Richard <richard.yang.tong@gmail.com>
2021-06-11 20:35:20 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/CustomerForQuery.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/CustomerForQuery.java
@@ -1,6 +1,10 @@
 package com.dragontalker.preparedstatement.crud;
 
+import com.dragontalker.util.JDBCUtils;
 import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 /**
  * 针对于Customer表的查询操作
@@ -10,6 +14,9 @@ public class CustomerForQuery {
 
     @Test
     public void testQuery1() {
+        Connection conn = JDBCUtils.getConnection();
+        String sql = "select id, name, email, birth from customers where id = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
 
     }
 }
