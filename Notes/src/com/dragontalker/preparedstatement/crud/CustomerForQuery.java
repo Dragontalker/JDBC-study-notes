9e28e28bd46ffe8015038c29eb168c6d136844b2
Richard <richard.yang.tong@gmail.com>
2021-06-12 18:53:59 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/CustomerForQuery.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/CustomerForQuery.java
@@ -26,26 +26,26 @@ public class CustomerForQuery {
             ps = conn.prepareStatement(sql);
             ps.setObject(1, 1);
 
-            //执行并返回结果集
+            //1. 执行并返回结果集
             resultSet = ps.executeQuery();
 
-            //处理结果集
+            //2. 处理结果集
             if(resultSet.next()) {//判断结果集的下一条是否有数据, 如果有数据, 返回true并指针下移, 如果false直接结束
 
-                //获取当前数据各个字段值
+                //3. 获取当前数据各个字段值
                 int id = resultSet.getInt(1);
                 String name = resultSet.getString(2);
                 String email = resultSet.getString(3);
                 Date birth = resultSet.getDate(4);
 
-                //将数据封装成一个对象
+                //4. 将数据封装成一个对象
                 Customer customer = new Customer(id, name, email, birth);
                 System.out.println(customer);
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            //关闭资源
+            //5. 关闭资源
             JDBCUtils.closeResource(conn, ps, resultSet);
         }
     }
