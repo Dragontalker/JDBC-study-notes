2cc46485dde366c15bf45b0680221a4b95b7802d
Richard <richard.yang.tong@gmail.com>
2021-06-11 20:21:58 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/PreparedStatementUpdateTest.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/PreparedStatementUpdateTest.java
@@ -21,7 +21,20 @@ import java.util.Properties;
 public class PreparedStatementUpdateTest {
 
     //通用的增删改操作
-    public void update() {
+    public void update(String sql, Object ... args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, ps);
+        }
 
     }
 
