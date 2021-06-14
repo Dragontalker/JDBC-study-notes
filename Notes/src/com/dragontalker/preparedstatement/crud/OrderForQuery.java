cc5f7a4cc0c454742ae80e6033ab3ee5dfed22ce
Richard <richard.yang.tong@gmail.com>
2021-06-14 12:10:24 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/OrderForQuery.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/OrderForQuery.java
@@ -1,8 +1,15 @@
 package com.dragontalker.preparedstatement.crud;
 
+import org.junit.Test;
+
 /**
  * @Description 针对orders表的查询操作
  */
 
 public class OrderForQuery {
+
+    @Test
+    public void testQuery1() {
+
+    }
 }
