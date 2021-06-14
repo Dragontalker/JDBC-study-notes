94a59d477603c390ddfc3feb14203a21ba53510a
Richard <richard.yang.tong@gmail.com>
2021-06-14 12:09:35 -0400

    Added notes.

--- Notes/src/com/dragontalker/bean/Order.java
+++ Notes/src/com/dragontalker/bean/Order.java
@@ -6,4 +6,12 @@ public class Order {
     private int orderId;
     private String orderName;
     private Date orderDate;
+
+    public Order() {}
+
+    public Order(int orderId, String orderName, Date orderDate) {
+        this.orderId = orderId;
+        this.orderName = orderName;
+        this.orderDate = orderDate;
+    }
 }
