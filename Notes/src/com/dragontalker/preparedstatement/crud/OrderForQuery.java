570b143c3cce2d6e09e642f4398c5870d55f495d
Richard <richard.yang.tong@gmail.com>
2021-06-14 12:11:59 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/OrderForQuery.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/OrderForQuery.java
@@ -4,6 +4,8 @@ import com.dragontalker.util.JDBCUtils;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * @Description 针对orders表的查询操作
@@ -14,5 +16,11 @@ public class OrderForQuery {
     @Test
     public void testQuery1() throws Exception{
         Connection conn = JDBCUtils.getConnection();
+        String sql = "select order_id, order_name, order_date from `orders` where id = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setObject(1, 1);
+
+        ResultSet rs = ps.executeQuery();
+
     }
 }
