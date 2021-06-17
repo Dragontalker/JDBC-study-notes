3207f19b2c234dbc27668deb5346ffb64a89da8b
Richard <richard.yang.tong@gmail.com>
2021-06-17 19:27:41 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/PreparedStatementQueryTest.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/PreparedStatementQueryTest.java
@@ -6,16 +6,61 @@ import com.dragontalker.util.JDBCUtils;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description 使用PreparedStatement来实现针对于不同表的通用的查询操作
  */
 
 public class PreparedStatementQueryTest {
+
+    public <T> List<T> getForList(Class<T> clazz, String sql, Object ... args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            ps = conn.prepareStatement(sql);
+            for(int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            //执行结果集
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            //获取列数
+            int columnCount = rsmd.getColumnCount();
+            //创建集合对象
+            ArrayList<T> list = new ArrayList<>();
+            while(rs.next()){
+                T t = clazz.newInstance();
+                //处理结果集一行数据中的每一个列: 给t对象指定的属性赋值
+                for(int i = 0; i < columnCount; i++) {
+                    //获取每个列的列值: 通过ResultSet
+                    Object columnValue = rs.getObject(i + 1);
+                    //获取每个列的列名: 通过ResultSetMetaData
+                    //获取列的别名: getColumnLabel()
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    //通过反射, 将对象指定名的ColumnName的属性赋值为指定的值ColumnValue
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columnValue);
+                }
+
+                list.add(t);
+            }
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, ps, rs);
+        }
+
+        return null;
+    }
 
     @Test
     public void testGetInstance() {
