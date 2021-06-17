71e45473f512063299ab280769d9d7a5330ced1f
Richard <richard.yang.tong@gmail.com>
2021-06-17 18:56:41 -0400

    Added notes.

--- Notes/src/com/dragontalker/preparedstatement/crud/PreparedStatementQueryTest.java
+++ Notes/src/com/dragontalker/preparedstatement/crud/PreparedStatementQueryTest.java
@@ -1,5 +1,14 @@
 package com.dragontalker.preparedstatement.crud;
 
+import com.dragontalker.bean.Order;
+import com.dragontalker.util.JDBCUtils;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
 /**
  * @Description 使用PreparedStatement来实现针对于不同表的通用的查询操作
  */
@@ -7,6 +16,44 @@ package com.dragontalker.preparedstatement.crud;
 public class PreparedStatementQueryTest {
 
     public Object getInstance(String sql, Object ... args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            ps = conn.prepareStatement(sql);
+            for(int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            //执行结果集
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            //获取列数
+            int columnCount = rsmd.getColumnCount();
+            if(rs.next()){
+                Order order = new Order();
+                for(int i = 0; i < columnCount; i++) {
+                    //获取每个列的列值: 通过ResultSet
+                    Object columnValue = rs.getObject(i + 1);
+                    //获取每个列的列名: 通过ResultSetMetaData
+                    //获取列的别名: getColumnLabel()
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    //通过反射, 将对象指定名的ColumnName的属性赋值为指定的值ColumnValue
+                    Field field = Order.class.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(order, columnValue);
+                }
+
+                return order;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, ps, rs);
+        }
+
         return null;
     }
 }
