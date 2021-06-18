bc5ff570fde28817fa77d09257f7894740f9c997
Richard <richard.yang.tong@gmail.com>
2021-06-18 15:58:21 -0400

    Added Notes.

---
+++ JDBC_02/src/com/dragontalker/daoplus/junit/CustomerDAOImplTest.java
@@ -0,0 +1,115 @@
+package com.dragontalker.daoplus.junit;
+
+import com.dragontalker.bean.Customer;
+import com.dragontalker.dao.CustomerDAOImpl;
+import com.dragontalker.utils.JDBCUtils;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.util.List;
+
+public class CustomerDAOImplTest {
+
+    private CustomerDAOImpl dao = new CustomerDAOImpl();
+
+    @Test
+    public void testInsert() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            Customer cust = new Customer(1, "YuXiaoFei", "xiaofei@126.com", new Date(43534646435L));
+            dao.insert(conn, cust);
+            System.out.println("添加成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    @Test
+    public void testDeleteById() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            dao.deleteById(conn, 13);
+            System.out.println("删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    @Test
+    public void testUpdateById() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            Customer cust = new Customer(18, "Mozart", "mozhate@126.com", new Date(43534646435L));
+            dao.updateById(conn, cust);
+            System.out.println("修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    @Test
+    public void testGetCustomerById() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            Customer cust = dao.getCustomerById(conn, 19);
+            System.out.println(cust);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    @Test
+    public void testGetAll() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            List<Customer> list = dao.getAll(conn);
+            list.forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    @Test
+    public void testGetCount() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            Long count = dao.getCount(conn);
+            System.out.println("表中的记录数为: " + count);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    @Test
+    public void testGetMaxBirth() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            Date maxBirth = dao.getMaxBirth(conn);
+            System.out.println(maxBirth);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+}
