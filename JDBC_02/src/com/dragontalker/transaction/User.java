609de3a8fde707b301cd6860b54542efbf595937
Richard <richard.yang.tong@gmail.com>
2021-06-18 11:21:08 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/transaction/User.java
+++ JDBC_02/src/com/dragontalker/transaction/User.java
@@ -12,4 +12,37 @@ public class User {
         this.password = password;
         this.balance = balance;
     }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user='" + user + '\'' +
+                ", password='" + password + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
