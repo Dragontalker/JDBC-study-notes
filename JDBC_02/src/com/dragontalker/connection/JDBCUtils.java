6f8d9058a545cd1f2fdd020ee47c10a15e0551a3
Richard <richard.yang.tong@gmail.com>
2021-06-18 19:58:21 -0400

    Added notes.

--- JDBC_02/src/com/dragontalker/connection/JDBCUtils.java
+++ JDBC_02/src/com/dragontalker/connection/JDBCUtils.java
@@ -1,9 +1,14 @@
 package com.dragontalker.connection;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.apache.commons.dbcp.BasicDataSourceFactory;
 
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class JDBCUtils {
 
@@ -12,5 +17,26 @@ public class JDBCUtils {
 
     public static Connection getConnection1() throws SQLException {
         return cpds.getConnection();
+    }
+
+    /**
+     * @Description 使用DBCP数据库连接池技术获取数据库连接
+     * @return
+     * @throws Exception
+     */
+    private static DataSource source;
+    static {
+        try {
+            Properties props = new Properties();
+            FileInputStream is = new FileInputStream(new File("src\\dbcp.properties"));
+            props.load(is);
+            source = BasicDataSourceFactory.createDataSource(props);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection2() throws Exception{
+        return source.getConnection();
     }
 }
