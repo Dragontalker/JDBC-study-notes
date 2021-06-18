09116d0f4ad6d14e0f6c0fb06080f666d3844374
Richard <richard.yang.tong@gmail.com>
2021-06-18 19:35:47 -0400

    Added notes.

--- JDBC_02/src/com/dragontalker/connection/DBCPTest.java
+++ JDBC_02/src/com/dragontalker/connection/DBCPTest.java
@@ -1,13 +1,20 @@
 package com.dragontalker.connection;
 
+import org.apache.commons.dbcp.BasicDataSource;
+
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class DBCPTest {
 
     /**
      * @Description 测试DBCP的数据库连接池技术
      */
-    public void testGetConnection() {
-        DataSource source = null;
+    public void testGetConnection() throws SQLException {
+        //创建了DBCP的数据库连接池
+        DataSource source = new BasicDataSource();
+
+        Connection conn = source.getConnection();
     }
 }
