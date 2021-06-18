216e36512a4698382c6aa1d6881b47c0b36d683d
Richard <richard.yang.tong@gmail.com>
2021-06-18 12:00:58 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/dao/CustomerDAO.java
+++ JDBC_02/src/com/dragontalker/dao/CustomerDAO.java
@@ -4,5 +4,10 @@ package com.dragontalker.dao;
 此接口用于规范针对于customers表的常用操作
  */
 
+import com.dragontalker.bean.Customer;
+
+import java.sql.Connection;
+
 public interface CustomerDAO {
+    void insert(Connection conn, Customer cust);
 }
