234298587d5b91ef8b28f6736dde0a99e17c17e6
Richard <richard.yang.tong@gmail.com>
2021-06-18 06:21:46 -0400

    Added notes.

--- Notes/src/com/dragontalker/blob/BlobTest.java
+++ Notes/src/com/dragontalker/blob/BlobTest.java
@@ -4,6 +4,7 @@ import com.dragontalker.util.JDBCUtils;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 /**
  * @Description 测试使用PreparedStatement操作Blob类型的数据
@@ -15,5 +16,8 @@ public class BlobTest {
     @Test
     public void testInsert() throws Exception{
         Connection conn = JDBCUtils.getConnection();
+
+        String sql = "insert into customers(name, email, birth, photo) values(?, ?, ?, ?)";
+        PreparedStatement ps = conn.prepareStatement(sql);
     }
 }
