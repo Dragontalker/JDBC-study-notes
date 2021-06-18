228dbf5606fb32adb94203b3d9b0a1d2a5736f21
Richard <richard.yang.tong@gmail.com>
2021-06-17 20:30:54 -0400

    Added notes.

---
+++ Notes/src/com/dragontalker/exer/Exercise1Test.java
@@ -0,0 +1,26 @@
+package com.dragontalker.exer;
+
+import com.dragontalker.util.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+//课后练习1
+public class Exercise1Test {
+    //通用的增删改操作
+    public void update(String sql, Object ... args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, ps);
+        }
+    }
+}
