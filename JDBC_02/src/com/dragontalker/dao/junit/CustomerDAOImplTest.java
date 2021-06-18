1e43459d5bc5bf21451ef0ad7f0acca32b0fceaf
Richard <richard.yang.tong@gmail.com>
2021-06-18 15:28:56 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/dao/junit/CustomerDAOImplTest.java
+++ JDBC_02/src/com/dragontalker/dao/junit/CustomerDAOImplTest.java
@@ -1,4 +1,29 @@
 package com.dragontalker.dao.junit;
 
+import com.dragontalker.bean.Customer;
+import com.dragontalker.dao.CustomerDAOImpl;
+import com.dragontalker.utils.JDBCUtils;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.Date;
+
 public class CustomerDAOImplTest {
+
+    private CustomerDAOImpl dao = new CustomerDAOImpl();
+
+    @Test
+    public void testInsert() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            Customer cust = new Customer(1, "YuXiaoFei", "xiaofei@126.com", new Date(43534646435L));
+            dao.insert(conn, cust);
+            System.out.println("添加成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
 }
