772fb93609e600c763c57cfe19fbcfd7e2ebe2c5
Richard <richard.yang.tong@gmail.com>
2021-06-18 08:11:58 -0400

    Added Notes.

---
+++ JDBC_02/src/com/dragontalker/utils/JDBCUtils.java
@@ -0,0 +1,21 @@
+package com.dragontalker.utils;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+public class JDBCUtils {
+
+    public static Connection getConnection() throws Exception{
+        InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("jdbc.properties");
+        Properties props = new Properties();
+        props.load(is);
+        String url = props.getProperty("url");
+        String user = props.getProperty("user");
+        String password = props.getProperty("password");
+        String driverClass = props.getProperty("driverClass");
+        Class.forName(driverClass);
+        return DriverManager.getConnection(url, user, password);
+    }
+}
