eec978cca123ad05aa678c6b0f6514055a87a58b
Richard <richard.yang.tong@gmail.com>
2021-06-18 06:43:08 -0400

    Added notes.

--- Notes/src/com/dragontalker/blob/BlobTest.java
+++ Notes/src/com/dragontalker/blob/BlobTest.java
@@ -4,12 +4,8 @@ import com.dragontalker.bean.Customer;
 import com.dragontalker.util.JDBCUtils;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.io.*;
+import java.sql.*;
 
 /**
  * @Description 测试使用PreparedStatement操作Blob类型的数据
@@ -39,27 +35,66 @@ public class BlobTest {
 
     //查询数据表customers中Blob类型的字段
     @Test
-    public void testQuery() throws Exception{
-        Connection conn = JDBCUtils.getConnection();
-        String sql = "select id, name, email, birth, photo from customers where id = ?";
-        PreparedStatement ps = conn.prepareStatement(sql);
-        ps.setInt(1, 21);
+    public void testQuery() {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        InputStream is = null;
+        FileOutputStream fos = null;
+        ResultSet rs = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            String sql = "select id, name, email, birth, photo from customers where id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, 21);
 
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()) {
-            //方式一:
-//            int id = rs.getInt(1);
-//            String name = rs.getString(2);
-//            String email = rs.getString(3);
-//            Date birth = rs.getDate(4);
+            is = null;
+            fos = null;
 
-            //方式二:
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            String email = rs.getString("email");
-            Date birth = rs.getDate("birth");
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                //方式一:
+    //            int id = rs.getInt(1);
+    //            String name = rs.getString(2);
+    //            String email = rs.getString(3);
+    //            Date birth = rs.getDate(4);
 
-            Customer cust = new Customer(id, name, email, birth);
+                //方式二:
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Date birth = rs.getDate("birth");
+
+                Customer cust = new Customer(id, name, email, birth);
+                System.out.println(cust);
+
+                //将Blob类型的字段下载下来, 以文件的方式保存在本地
+                Blob photo = rs.getBlob("photo");
+                is = photo.getBinaryStream();
+                fos = new FileOutputStream("liuyan.jpg");
+                byte[] buffer = new byte[1024];
+                int len;
+                while((len = is.read(buffer)) != -1) {
+                    fos.write(buffer, 0, len);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, ps, rs);
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
