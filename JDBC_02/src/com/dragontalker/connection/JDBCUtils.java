e516cb7af5e9c5eaa3da537a6875a61175d7a8f2
Richard <richard.yang.tong@gmail.com>
2021-06-18 17:00:52 -0400

    Added notes.

--- JDBC_02/src/com/dragontalker/connection/JDBCUtils.java
+++ JDBC_02/src/com/dragontalker/connection/JDBCUtils.java
@@ -7,8 +7,10 @@ import java.sql.SQLException;
 
 public class JDBCUtils {
 
+    //数据库连接池只需提供一个即可
+    private static ComboPooledDataSource cpds = new ComboPooledDataSource("hello-c3p0");
+
     public static Connection getConnection1() throws SQLException {
-        ComboPooledDataSource cpds = new ComboPooledDataSource("hello-c3p0");
         return cpds.getConnection();
     }
 }
