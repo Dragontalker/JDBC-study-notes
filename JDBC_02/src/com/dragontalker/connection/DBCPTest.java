997b205ecf762d912533ec3f6b781d6f6dd1157b
Richard <richard.yang.tong@gmail.com>
2021-06-18 19:41:41 -0400

    Added notes.

--- JDBC_02/src/com/dragontalker/connection/DBCPTest.java
+++ JDBC_02/src/com/dragontalker/connection/DBCPTest.java
@@ -1,8 +1,8 @@
 package com.dragontalker.connection;
 
 import org.apache.commons.dbcp.BasicDataSource;
+import org.junit.Test;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -11,13 +11,14 @@ public class DBCPTest {
     /**
      * @Description 测试DBCP的数据库连接池技术
      */
+    @Test
     public void testGetConnection() throws SQLException {
         //创建了DBCP的数据库连接池
         BasicDataSource source = new BasicDataSource();
 
         //设置基本信息
         source.setDriverClassName("com.mysql.jdbc.Driver");
-        source.setUrl("jdbc:mysql:///test");
+        source.setUrl("jdbc:mysql://localhost:3306/test?characterEncoding=latin1");
         source.setUsername("root");
         source.setPassword("password");
 
