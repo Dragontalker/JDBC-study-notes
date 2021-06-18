5f2f9f593fa612fd60b0b6dc323f524b8c0f826c
Richard <richard.yang.tong@gmail.com>
2021-06-18 12:02:12 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/dao/CustomerDAO.java
+++ JDBC_02/src/com/dragontalker/dao/CustomerDAO.java
@@ -9,5 +9,10 @@ import com.dragontalker.bean.Customer;
 import java.sql.Connection;
 
 public interface CustomerDAO {
+    /**
+     * @Description 将cust对象添加到数据库中
+     * @param conn
+     * @param cust
+     */
     void insert(Connection conn, Customer cust);
 }
