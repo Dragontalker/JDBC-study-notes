d1eb918b9fbecd9e599d63f2857b3e626be921bc
Richard <richard.yang.tong@gmail.com>
2021-06-18 17:08:01 -0400

    Added notes.

--- JDBC_02/src/com/dragontalker/connection/DBCPTest.java
+++ JDBC_02/src/com/dragontalker/connection/DBCPTest.java
@@ -1,4 +1,13 @@
 package com.dragontalker.connection;
 
+import javax.sql.DataSource;
+
 public class DBCPTest {
+
+    /**
+     * @Description 测试DBCP的数据库连接池技术
+     */
+    public void testGetConnection() {
+        DataSource source = null;
+    }
 }
