ce3e6b36242d01a338035df86135e16b736b0ba7
Richard <richard.yang.tong@gmail.com>
2021-06-18 16:56:09 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/connection/JDBCUtils.java
+++ JDBC_02/src/com/dragontalker/connection/JDBCUtils.java
@@ -1,4 +1,14 @@
 package com.dragontalker.connection;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class JDBCUtils {
+
+    public static Connection getConnection1() throws SQLException {
+        ComboPooledDataSource cpds = new ComboPooledDataSource("hello-c3p0");
+        return cpds.getConnection();
+    }
 }
