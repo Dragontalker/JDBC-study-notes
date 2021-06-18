dbba9aa6d40272ab4a0223898b4050ae3c09b9af
Richard <richard.yang.tong@gmail.com>
2021-06-18 11:50:30 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/dao/BaseDAO.java
+++ JDBC_02/src/com/dragontalker/dao/BaseDAO.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 封装了对于数据表的通用的操作
@@ -60,6 +62,50 @@ public class BaseDAO {
 
                 return t;
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+        }
+
+        return null;
+    }
+
+    public <T> List<T> getForList(Connection conn, Class<T> clazz, String sql, Object ... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for(int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            //执行结果集
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            //获取列数
+            int columnCount = rsmd.getColumnCount();
+            //创建集合对象
+            ArrayList<T> list = new ArrayList<>();
+            while(rs.next()){
+                T t = clazz.newInstance();
+                //处理结果集一行数据中的每一个列: 给t对象指定的属性赋值
+                for(int i = 0; i < columnCount; i++) {
+                    //获取每个列的列值: 通过ResultSet
+                    Object columnValue = rs.getObject(i + 1);
+                    //获取每个列的列名: 通过ResultSetMetaData
+                    //获取列的别名: getColumnLabel()
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    //通过反射, 将对象指定名的ColumnName的属性赋值为指定的值ColumnValue
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columnValue);
+                }
+
+                list.add(t);
+            }
+            return list;
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
