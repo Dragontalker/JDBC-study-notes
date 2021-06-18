bfe87673078bde554c3646b23bd85f982e6ed10f
Richard <richard.yang.tong@gmail.com>
2021-06-18 08:25:15 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/transaction/TransactionTest.java
+++ JDBC_02/src/com/dragontalker/transaction/TransactionTest.java
@@ -1,4 +1,12 @@
 package com.dragontalker.transaction;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class TransactionTest {
+
+    public void update(String sql, Object ... args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+    }
 }
