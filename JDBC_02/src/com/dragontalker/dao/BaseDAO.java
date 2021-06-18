9b508f2a523b21bdd1404de494a71df9790cb58b
Richard <richard.yang.tong@gmail.com>
2021-06-18 11:49:26 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/dao/BaseDAO.java
+++ JDBC_02/src/com/dragontalker/dao/BaseDAO.java
@@ -2,8 +2,11 @@ package com.dragontalker.dao;
 
 import com.dragontalker.utils.JDBCUtils;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 /*
 封装了对于数据表的通用的操作
@@ -24,5 +27,45 @@ public class BaseDAO {
             JDBCUtils.closeResource(null, ps);
         }
         return 0;
+    }
+
+    public <T> T getInstance(Connection conn, Class<T> clazz, String sql, Object ... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for(int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            //执行结果集
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            //获取列数
+            int columnCount = rsmd.getColumnCount();
+            if(rs.next()){
+                T t = clazz.newInstance();
+                for(int i = 0; i < columnCount; i++) {
+                    //获取每个列的列值: 通过ResultSet
+                    Object columnValue = rs.getObject(i + 1);
+                    //获取每个列的列名: 通过ResultSetMetaData
+                    //获取列的别名: getColumnLabel()
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    //通过反射, 将对象指定名的ColumnName的属性赋值为指定的值ColumnValue
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columnValue);
+                }
+
+                return t;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+        }
+
+        return null;
     }
 }
--- JDBC_02/src/com/dragontalker/transaction/TransactionTest.java
+++ JDBC_02/src/com/dragontalker/transaction/TransactionTest.java
@@ -215,5 +215,4 @@ public class TransactionTest {
 
         return null;
     }
-
 }
