cbdc0d21021ce55a935a427f34c4d6846dd53348
Richard <richard.yang.tong@gmail.com>
2021-06-18 11:48:59 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/dao/BaseDAO.java
+++ JDBC_02/src/com/dragontalker/dao/BaseDAO.java
@@ -1,7 +1,28 @@
 package com.dragontalker.dao;
 
+import com.dragontalker.utils.JDBCUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 /*
 封装了对于数据表的通用的操作
  */
 public class BaseDAO {
+
+    public int update(Connection conn, String sql, Object ... args) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps);
+        }
+        return 0;
+    }
 }
