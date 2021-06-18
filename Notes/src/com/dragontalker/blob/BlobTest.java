2e2551220825679e581654de3ec4ebdc68ec5c5f
Richard <richard.yang.tong@gmail.com>
2021-06-18 06:20:33 -0400

    Added notes.

--- Notes/src/com/dragontalker/blob/BlobTest.java
+++ Notes/src/com/dragontalker/blob/BlobTest.java
@@ -1,8 +1,16 @@
 package com.dragontalker.blob;
 
+import org.junit.Test;
+
 /**
  * @Description 测试使用PreparedStatement操作Blob类型的数据
  */
 
 public class BlobTest {
+
+    //向数据表customers中插入Blob类型的字段
+    @Test
+    public void testInsert() {
+
+    }
 }
