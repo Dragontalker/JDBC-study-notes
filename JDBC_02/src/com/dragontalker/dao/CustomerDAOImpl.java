1e271994637ff9458094fb8bd3a86178075dd1a6
Richard <richard.yang.tong@gmail.com>
2021-06-18 12:08:25 -0400

    Added Notes.

---
+++ JDBC_02/src/com/dragontalker/dao/CustomerDAOImpl.java
@@ -0,0 +1,44 @@
+package com.dragontalker.dao;
+
+import com.dragontalker.bean.Customer;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.util.List;
+
+public class CustomerDAOImpl extends BaseDAO implements CustomerDAO{
+    @Override
+    public void insert(Connection conn, Customer cust) {
+
+    }
+
+    @Override
+    public void deleteById(Connection conn, int id) {
+
+    }
+
+    @Override
+    public void updateById(Connection conn, Customer cust) {
+
+    }
+
+    @Override
+    public void getCustomerById(Connection conn, int id) {
+
+    }
+
+    @Override
+    public List<Customer> getAll(Connection conn) {
+        return null;
+    }
+
+    @Override
+    public Long getCount(Connection conn) {
+        return null;
+    }
+
+    @Override
+    public Date getMaxBirth(Connection conn) {
+        return null;
+    }
+}
