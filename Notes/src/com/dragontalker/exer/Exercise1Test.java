f5792fd6cf64009e2ebb75b675a73b0bad589e16
Richard <richard.yang.tong@gmail.com>
2021-06-17 20:32:33 -0400

    Added notes.

--- Notes/src/com/dragontalker/exer/Exercise1Test.java
+++ Notes/src/com/dragontalker/exer/Exercise1Test.java
@@ -1,12 +1,22 @@
 package com.dragontalker.exer;
 
 import com.dragontalker.util.JDBCUtils;
+import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.Scanner;
 
 //课后练习1
 public class Exercise1Test {
+
+    @Test
+    public void testInsert() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入用户名");
+        String name = scanner.next();
+    }
+
     //通用的增删改操作
     public void update(String sql, Object ... args) {
         Connection conn = null;
