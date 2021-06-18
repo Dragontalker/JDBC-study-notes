f70d56ee54964266a267bdfb01e2a1b6d8b8769c
Richard <richard.yang.tong@gmail.com>
2021-06-18 11:18:09 -0400

    Added Notes.

--- JDBC_02/src/com/dragontalker/transaction/TransactionTest.java
+++ JDBC_02/src/com/dragontalker/transaction/TransactionTest.java
@@ -146,6 +146,11 @@ public class TransactionTest {
 
     }
 
+    @Test
+    public void testTransactionUpdate() {
+
+    }
+
     //通用的查询操作, 用于返回数据表中的一条记录
     //version 2.0 考虑上事务
     public <T> List<T> getForList(Connection conn, Class<T> clazz, String sql, Object ... args) {
