a203c0c9858648d373c6a0fa87a33e01ffc35da6
Richard <richard.yang.tong@gmail.com>
2021-06-18 06:20:54 -0400

    Added notes.

--- Notes/src/com/dragontalker/blob/BlobTest.java
+++ Notes/src/com/dragontalker/blob/BlobTest.java
@@ -1,6 +1,9 @@
 package com.dragontalker.blob;
 
+import com.dragontalker.util.JDBCUtils;
 import org.junit.Test;
+
+import java.sql.Connection;
 
 /**
  * @Description 测试使用PreparedStatement操作Blob类型的数据
@@ -10,7 +13,7 @@ public class BlobTest {
 
     //向数据表customers中插入Blob类型的字段
     @Test
-    public void testInsert() {
-
+    public void testInsert() throws Exception{
+        Connection conn = JDBCUtils.getConnection();
     }
 }
