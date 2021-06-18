7c32afcfc01dbcffad1ceef1b697f35954d58461
Richard <richard.yang.tong@gmail.com>
2021-06-18 15:44:33 -0400

    Added Notes.

---
+++ JDBC_02/src/com/dragontalker/daoplus/BaseDAO.java
@@ -0,0 +1,137 @@
+package com.dragontalker.daoplus;
+
+import com.dragontalker.utils.JDBCUtils;
+
+import java.lang.reflect.Field;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+DAO: data(base) access object
+封装了对于数据表的通用的操作
+ */
+public abstract class BaseDAO {
+
+    public int update(Connection conn, String sql, Object ... args) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps);
+        }
+        return 0;
+    }
+
+    public <T> T getInstance(Connection conn, Class<T> clazz, String sql, Object ... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for(int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            //执行结果集
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            //获取列数
+            int columnCount = rsmd.getColumnCount();
+            if(rs.next()){
+                T t = clazz.newInstance();
+                for(int i = 0; i < columnCount; i++) {
+                    //获取每个列的列值: 通过ResultSet
+                    Object columnValue = rs.getObject(i + 1);
+                    //获取每个列的列名: 通过ResultSetMetaData
+                    //获取列的别名: getColumnLabel()
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    //通过反射, 将对象指定名的ColumnName的属性赋值为指定的值ColumnValue
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columnValue);
+                }
+
+                return t;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+        }
+
+        return null;
+    }
+
+    public <T> List<T> getForList(Connection conn, Class<T> clazz, String sql, Object ... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for(int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            //执行结果集
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            //获取列数
+            int columnCount = rsmd.getColumnCount();
+            //创建集合对象
+            ArrayList<T> list = new ArrayList<>();
+            while(rs.next()){
+                T t = clazz.newInstance();
+                //处理结果集一行数据中的每一个列: 给t对象指定的属性赋值
+                for(int i = 0; i < columnCount; i++) {
+                    //获取每个列的列值: 通过ResultSet
+                    Object columnValue = rs.getObject(i + 1);
+                    //获取每个列的列名: 通过ResultSetMetaData
+                    //获取列的别名: getColumnLabel()
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+
+                    //通过反射, 将对象指定名的ColumnName的属性赋值为指定的值ColumnValue
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columnValue);
+                }
+
+                list.add(t);
+            }
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+        }
+
+        return null;
+    }
+
+    //用于查询特殊值的通用的方法
+    public <E> E getValue(Connection conn, String sql, Object ... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            if(rs.next()) {
+                return (E) rs.getObject(1);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+        }
+        return null;
+    }
+}
---
+++ JDBC_02/src/com/dragontalker/daoplus/CustomerDAO.java
@@ -0,0 +1,62 @@
+package com.dragontalker.daoplus;
+
+/*
+此接口用于规范针对于customers表的常用操作
+ */
+
+import com.dragontalker.bean.Customer;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.util.List;
+
+public interface CustomerDAO {
+    /**
+     * @Description 将cust对象添加到数据库中
+     * @param conn
+     * @param cust
+     */
+    void insert(Connection conn, Customer cust);
+
+    /**
+     * @Description 根据指定的id, 删除表中的一条记录
+     * @param conn
+     * @param id
+     */
+    void deleteById(Connection conn, int id);
+
+    /**
+     * @Description 针对内存中的cust对象, 去修改数据表中指定的记录
+     * @param conn
+     * @param cust
+     */
+    void updateById(Connection conn, Customer cust);
+
+    /**
+     * @Description 根据指定的id查询得到对应的Customer对象
+     * @param conn
+     * @param id
+     */
+    Customer getCustomerById(Connection conn, int id);
+
+    /**
+     * @Description 查询表中的所有记录构成的集合
+     * @param conn
+     * @return
+     */
+    List<Customer> getAll(Connection conn);
+
+    /**
+     * @Description 返回数据表中数据的条目数
+     * @param conn
+     * @return
+     */
+    Long getCount(Connection conn);
+
+    /**
+     * @Description 返回数据表中最大的生日
+     * @param conn
+     * @return
+     */
+    Date getMaxBirth(Connection conn);
+}
