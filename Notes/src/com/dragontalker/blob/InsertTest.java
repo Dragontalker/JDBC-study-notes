3e3cfa8ba3c42efee273b0203dc7bee53572d78b
Richard <richard.yang.tong@gmail.com>
2021-06-18 06:56:53 -0400

    Added notes.

--- Notes/src/com/dragontalker/blob/InsertTest.java
+++ Notes/src/com/dragontalker/blob/InsertTest.java
@@ -6,7 +6,15 @@ package com.dragontalker.blob;
  * update, delete本身就具有批量操作的效果
  * 此时的批量操作, 主要指的是批量插入, 使用PreparedStatement如何实现更高效的批量插入?
  *
+ * 题目: 要求向goods表中插入2000条数据
  *
+ * CREATE TABLE goods(
+ * 	id INT PRIMARY KEY AUTO_INCREMENT,
+ *     NAME VARCHAR(25)
+ * );
+ *
+ *
+ * 方式一:
  */
 
 public class InsertTest {
