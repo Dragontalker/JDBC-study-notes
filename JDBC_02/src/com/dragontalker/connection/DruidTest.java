9a172393684636bce242524ba59a41183b2604ee
Richard <richard.yang.tong@gmail.com>
2021-06-18 20:13:36 -0400

    Added notes.

--- JDBC_02/src/com/dragontalker/connection/DruidTest.java
+++ JDBC_02/src/com/dragontalker/connection/DruidTest.java
@@ -1,10 +1,24 @@
 package com.dragontalker.connection;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.util.Properties;
 
 public class DruidTest {
 
-    public void getConnection() {
-        DruidDataSource source = new DruidDataSource();
+    @Test
+    public void getConnection() throws Exception{
+        Properties props = new Properties();
+        FileInputStream is = new FileInputStream(new File("src\\dbcp.properties"));        props.load(is);
+        DataSource source = DruidDataSourceFactory.createDataSource(props);
+        Connection conn = source.getConnection();
+        System.out.println(conn);
     }
 }
